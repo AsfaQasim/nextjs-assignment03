023489e50b5a9dba039ca30109688ebf43f42617
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-10-23 01:59:18 -0700

    first commit

---
+++ src/app/component/footer.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Footer(){
+    return(
+  <div className="bg-red-200 h-16">
+    <p className=" md:w-1/4 mb-6 md:mb-0 text-center">&copy; Made by Asifa M.Qasim</p>
+  </div>
+    )
+}
